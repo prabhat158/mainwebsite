95d9e3afb42690ccfdec9f6574a03144f46355c3
Anuj Agrawal <anujagrawal.official@gmail.com>
2019-09-04 23:04:02 +0530

    Reg form

--- src/app/registration/regform/regform.component.ts
+++ src/app/registration/regform/regform.component.ts
@@ -8,11 +8,29 @@ import {FormGroup} from '@angular/forms';
 })
 export class RegformComponent implements OnInit {
 
-  constructor() { }
+  list = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5', 'option 6', 'option 7', 'option 8', 'option 9'];
+  showDropDown = false;
+  college = 'Select Your College ';
+  inputOff = true;
+
+  constructor() {
+  }
 
   form = new FormGroup({});
 
   ngOnInit() {
   }
+  toggleDropDown() {
+    this.showDropDown = !this.showDropDown;
 
+  }
+  printAll() {
+
+  }
+  selectValue(value) {
+    this.college = value;
+    console.log(this.college);
+    this.showDropDown = false;
+    // this.stateForm.patchValue({search: value});
+  }
 }
