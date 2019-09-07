8c7c6908e65fee84dc31144c3bd71bac808e0eb4
prabhat158 <prabhatov158@gmail.com>
2019-09-08 04:26:21 +0530

    123

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'assets/js/animations.js';
+    // this.elementRef.nativeElement.appendChild(s);
+    document.body.appendChild(s);
   }
 
 }
