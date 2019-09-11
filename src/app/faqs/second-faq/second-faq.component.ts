d12c095182e7fc8ace52bbd98eeddd704aaee534
Pulkit <pulkitagrawal2000@gmail.com>
2019-09-11 18:09:48 +0530

    updated acco and faq

---
+++ src/app/faqs/second-faq/second-faq.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-faq',
+  templateUrl: './second-faq.component.html',
+  styleUrls: ['./second-faq.component.css']
+})
+export class SecondFaqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
