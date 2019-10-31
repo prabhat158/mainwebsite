dcdbc0056e075e86c5f3cc32e4b8e6a4ba91f621
Prabhat Singh <prabhatnov158@gmail.com>
2019-10-31 18:37:34 +0530

    log

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+if (environment.production) {
+  console.log = function () { return false; }
+}
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
