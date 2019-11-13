c3da30fb08178764036f17e4cde11db8fe8bda04
Prabhat Singh <prabhatnov158@gmail.com>
2019-11-13 10:58:11 +0530

    wne

--- src/app/events/wne/wne.component.ts
+++ src/app/events/wne/wne.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { EventdataService } from '../eventdata.service';
 
+import { DataService } from '../../registration/data.service';
 import { HttpClient } from '@angular/common/http';
 import { DomSanitizer} from '@angular/platform-browser';
 
@@ -13,9 +14,17 @@ import { DomSanitizer} from '@angular/platform-browser';
 export class WneComponent implements OnInit {
   wne: any;
   clickedButton;
+  status=false;
+
+  name=JSON.parse(this.profile.getJdata()).name;
+  email=JSON.parse(this.profile.getJdata()).email
+  mobile=JSON.parse(this.profile.getJdata()).mobile
+  mino=JSON.parse(this.profile.getJdata()).mi_no
 
   constructor(
+    private profile:DataService,
     private route: ActivatedRoute,
+    private router: Router,
     private dataService: EventdataService,
     private http:HttpClient,   
     protected sanitizer: DomSanitizer,
@@ -24,6 +33,9 @@ export class WneComponent implements OnInit {
       return this.sanitizer.bypassSecurityTrustHtml(type);
     }
 
+    sendTo(){
+      this.router.navigateByUrl('/register');
+    }
 
   getCategories(){
 
@@ -33,6 +45,7 @@ export class WneComponent implements OnInit {
       
         this.wne = data['Workshops'];
         this.clickedButton=this.wne[0].name;
+        this.wnedata(this.wne[0].events[0].id)
       this.dataService.setWnedetail(data['Workshops']);
 
       },
@@ -40,12 +53,48 @@ export class WneComponent implements OnInit {
     
     this.wne =this.dataService.getWnedetail();
     this.clickedButton=this.wne[0].name;
+    this.wnedata(this.wne[0].events[0].id)
   }
           // console.log(this.pronites_nites);
-  }  
+  }
 
-  onClick(button){
-    console.log(button);
+  wnereg(event){
+    this.http.post('https://api2.moodi.org/workshopreg', {
+      name:this.name,
+      mi_number:this.mino,
+      mobile_number:this.mobile,
+      event:event,
+      email:this.email
+     })
+      .subscribe(result =>{
+        this.wnedata(event)
+      },
+      data => {
+        console.log(data)
+        alert(JSON.stringify(data["error"]))
+        },
+        () => {
+        }
+      ); 
+  }
+  regdata
+  wnedata(id){
+    this.http.get('https://api2.moodi.org/workshopdetail/'+this.mino+'/id/'+id)
+      .subscribe(result =>{
+          this.regdata=result
+          console.log(result)
+      },
+      data => {
+        this.regdata=undefined
+        },
+        () => {
+        }
+      ); 
+  }
+  eventid;
+  onClick(button, id){
+    this.eventid=id;
+    this.wnedata(id)
     this.clickedButton = button;
   }
 
@@ -56,6 +105,12 @@ export class WneComponent implements OnInit {
         // this.current_pronites_nite = params.pronites
       }
     );
+
+    if(localStorage.getItem("jdata")===null){
+      this.status=false;
+    }else{
+      this.status=true;
+    }
   }
 
 }
