3ffb17ee9c928777dc5cde7ec6e932a3d0209241
Prabhat Singh <prabhatnov158@gmail.com>
2019-11-14 19:54:18 +0530

    loading

--- src/app/accommodation/accommodation.component.ts
+++ src/app/accommodation/accommodation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-accommodation',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accommodation.component.css']
 })
 export class AccommodationComponent implements OnInit {
+  public lottieConfig: Object;
+  private anim: any;
+  animationSpeed: number = 1;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    this.lottieConfig = {
+      path: 'assets/data.json',
+      autoplay: true,
+      loop: true
+  };
+  }
+  handleAnimation(anim: any) {
+    this.anim = anim;
+}
+  public ngOnInit()
+  {
+    $(window).on('load', function() { 
+      $(".se-pre-con").fadeOut("slow");});
+      console.log("here")
   }
 
 }
--- src/app/faqs/faqs.component.ts
+++ src/app/faqs/faqs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-faqs',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faqs.component.css']
 })
 export class FaqsComponent implements OnInit {
+  public lottieConfig: Object;
+  private anim: any;
+  animationSpeed: number = 1;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    this.lottieConfig = {
+      path: 'assets/data.json',
+      autoplay: true,
+      loop: true
+  };
   }
-
+  handleAnimation(anim: any) {
+    this.anim = anim;
+}
+  public ngOnInit()
+  {
+    $(window).on('load', function() { 
+      $(".se-pre-con").fadeOut("slow");});
+      console.log("here")
+  };
+  
 }
