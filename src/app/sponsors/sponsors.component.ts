336d90771319a21636e76b6e1958cc3968bfcee1
Aditya <sadityam123@gmail.com>
2019-12-21 02:58:12 +0530

    'dsfdbgnhgbfv'

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery'
 @Component({
   selector: 'app-sponsors',
   templateUrl: './sponsors.component.html',
@@ -10,6 +10,19 @@ export class SponsorsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(window).scroll(function(){
+      // console.log('wwww');
+      if ($(this).scrollTop() > 400) {
+        $('.top-scroller').fadeIn();
+      } else {
+        $('.top-scroller').fadeOut();
+      }
+    });
+    //Click event to scroll to top
+    $('.top-scroller').click(function(){
+      $('html, body').animate({scrollTop : 0},1000);
+      return false;
+    });
   }
 
 }
